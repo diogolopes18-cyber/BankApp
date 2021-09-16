ccc1b68a004eaf3af207d5f9ac549074a503d875
EU\diogolopes <dlopes@neoception.com>
2021-09-16 16:14:30 +0100

    (feature): add new class for file handling

---
+++ Bank/FileHandling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Bank
+{
+    public class FileHandling
+    {
+        public string username { get; set; }
+        
+        internal void SaveHistoryToFile(string username)
+        {
+            this.username = username;
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            //Create a new file
+            StreamWriter file = new StreamWriter(desktopPath + @"\test.txt");
+
+            //Write username to file
+            file.WriteLine(this.username);
+
+            //Close file
+            file.Close();
+
+        }
+
+
+    }
+}
